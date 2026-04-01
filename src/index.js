696ab5793ab95c5e1fe16b3d46a852a92c8c3206
daqingyang706-commits <daqingyang706@gmail.com>
2026-04-01 19:01:52 +0800

    index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CardEditor from './CardEditor';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CardEditor />
+  </React.StrictMode>
+);
